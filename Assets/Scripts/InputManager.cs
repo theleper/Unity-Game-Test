a869d6689075e36505b6ee7fb137decce0c3b3b4
theleper <yoav.levin@gmail.com>
2017-01-09 10:51:43 -0500

    Assets

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public enum Buttons
+{
+    Right,
+    Left,
+}
+
+public enum Condition
+{
+    GreaterThan,
+    LessThan
+}
+[System.Serializable]
+public class InputAxisState
+{
+    public string axisName;
+    public float offValue;
+    public Buttons button;
+    public Condition condition;
+
+    public bool value{
+        get{
+            var val = Input.GetAxis("Horizontal");
+
+            switch (condition)
+            {
+                case Condition.GreaterThan:
+                    return val > offValue;
+                case Condition.LessThan:
+                    return val < offValue;
+            }
+            return false;
+            }
+        }
+    }
+public class InputManager : MonoBehaviour {
+
+    public InputAxisState[] inputs;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (var input in inputs)
+        {
+            if(input.value)
+            {
+                Debug.Log("Input Detected" + input.button);
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/WeaponsManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum bulletType
+{
+    Simple,
+    Shotgun,
+    RocketLauncher
+}
+public class WeaponsManager : MonoBehaviour {
+
+    public bool[] WeaponsArray;
+	// Use this for initialization
+	void Start () {
+        WeaponsArray = new bool[System.Enum.GetValues(typeof(bulletType)).Length];
+        WeaponsArray[(int)bulletType.Simple] = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
